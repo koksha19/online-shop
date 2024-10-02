20e6364a8597a353bac5a17ab986f82a993495c3
Lev <lev.bereza@gmail.com>
2024-10-02 10:51:36 +0300

    Deleting old image from the file after editing or deleting the product

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 const { validationResult } = require("express-validator");
 
 const returnErrorPage = require("../middleware/returnErrorPage");
+const fileHelper = require("../util/file");
 
 exports.getAddProduct = (req, res) => {
   res.render("admin/edit-product", {
@@ -156,6 +157,7 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       if (image) {
+        fileHelper.deleteFile(product.imageUrl);
         product.imageUrl = image.path;
       }
       product.description = updatedDescription;
@@ -172,7 +174,14 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDelete = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteOne({ _id: prodId, userId: req.user._id })
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return next(new Error("Product not found"));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
     .then(() => {
       res.redirect("/admin/products");
     })
